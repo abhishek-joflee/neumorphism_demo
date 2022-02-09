4cac51dba046298919ed6fc4ba6aa140a9fea02b
abhishek-joflee <abhishek.patil@joflee.com>
2022-02-09 15:39:18 +0530

    Animated button added

--- lib/main.dart
+++ lib/main.dart
@@ -14,72 +14,140 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Neumorphism demo'),
+      home: MyHomePage(title: 'Neumorphism demo'),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key, required this.title}) : super(key: key);
 
   final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  late AnimationController _animationController;
+
+  @override
+  void initState() {
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey[300],
-      body: Center(
-        child: Container(
-          decoration: BoxDecoration(
-            shape: BoxShape.circle,
-            color: Colors.green,
-            boxShadow: [
-              // bottom right
-              BoxShadow(
-                color: Colors.grey.shade500,
-                offset: const Offset(5, 5),
-                blurRadius: 15,
-                spreadRadius: 1,
-              ),
+      body: const Center(
+        child: MyButton(),
+      ),
+    );
+  }
+}
 
-              // top left
-              const BoxShadow(
-                color: Colors.white,
-                offset: Offset(-5, -5),
-                blurRadius: 13,
-                spreadRadius: 1,
-              ),
-            ],
+class MyButton extends StatefulWidget {
+  const MyButton({Key? key}) : super(key: key);
 
-            // gradient for 3d effect
-            gradient: LinearGradient(
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-              colors: [
-                Colors.grey.shade200,
-                Colors.grey.shade300,
-                Colors.grey.shade400,
-                Colors.grey.shade500,
-              ],
-              stops: const [
-                0.1,
-                0.3,
-                0.8,
-                0.9,
-              ],
-            ),
-          ),
-          child: Material(
-            shape: const CircleBorder(),
-            color: Colors.transparent,
-            child: InkWell(
-              onTap: () {},
-              // splashColor: Colors.transparent,
-              hoverColor: Colors.transparent,
-              customBorder: const CircleBorder(),
-              child: const SizedBox(
-                width: 300,
-                height: 300,
+  @override
+  State<MyButton> createState() => _MyButtonState();
+}
+
+class _MyButtonState extends State<MyButton>
+    with SingleTickerProviderStateMixin {
+  bool isElevated = true;
+  bool isAnimating = false;
+  late AnimationController _animationController;
+
+  void changeIcon() {
+//rebuilds UI with changing icon.
+    setState(() {
+      isAnimating = !isAnimating;
+      isAnimating
+          ? _animationController.forward()
+          : _animationController.reverse();
+    });
+  }
+
+  @override
+  void initState() {
+    _animationController =
+        AnimationController(vsync: this, duration: Duration(milliseconds: 300));
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _animationController.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedContainer(
+      duration: const Duration(milliseconds: 100),
+      curve: Curves.easeInOut,
+      decoration: BoxDecoration(
+        shape: BoxShape.circle,
+        color: Colors.grey[300],
+        boxShadow: isElevated
+            ? [
+                // bottom right
+                BoxShadow(
+                  color: Colors.grey.shade500,
+                  offset: const Offset(5, 5),
+                  blurRadius: 15,
+                  spreadRadius: 1,
+                ),
+
+                // top left
+                const BoxShadow(
+                  color: Colors.white,
+                  offset: Offset(-5, -5),
+                  blurRadius: 13,
+                  spreadRadius: 1,
+                ),
+              ]
+            : [],
+      ),
+      child: Material(
+        shape: const CircleBorder(),
+        color: Colors.transparent,
+        child: MouseRegion(
+          cursor: SystemMouseCursors.click,
+          child: GestureDetector(
+            onTap: () {
+              setState(() {
+                changeIcon();
+              });
+            },
+            onTapDown: (_) {
+              setState(() {
+                isElevated = false;
+              });
+            },
+            onTapCancel: () {
+              setState(() {
+                isElevated = true;
+              });
+            },
+            onTapUp: (_) {
+              // do button tap operation here
+              setState(() {
+                isElevated = true;
+              });
+            },
+            child: Container(
+              width: 300,
+              height: 300,
+              color: Colors.transparent,
+              child: Center(
+                child: AnimatedIcon(
+                  size: 100,
+                  progress: _animationController,
+                  icon: AnimatedIcons.arrow_menu,
+                ),
               ),
             ),
           ),
